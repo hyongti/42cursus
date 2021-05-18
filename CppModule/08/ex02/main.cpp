b47f7900043c8d97306b78d7c8743ce5d3ed3d2b
hyongti <hyongtiii@gmail.com>
2021-05-18 18:05:56 +0900

    update ex02

--- CppModule/08/ex02/main.cpp
+++ CppModule/08/ex02/main.cpp
@@ -7,38 +7,6 @@
 
 int main()
 {
-	// std::stack<int>	mstack;
-
-	// mstack.push(5);
-	// mstack.push(17);
-
-	// std::cout << mstack.top() << std::endl;
-
-	// mstack.pop();
-
-	// std::cout << mstack.size() << std::endl;
-
-	// mstack.push(3);
-	// mstack.push(5);
-	// mstack.push(737);
-	// //[...]
-	// mstack.push(0);
-
-	// std::stack<int>::iterator it = mstack.begin();
-	// std::stack<int>::iterator ite = mstack.end();
-
-	// ++it;
-	// --it;
-	// while (it != ite)
-	// {
-	// 	std::cout << *it << std::endl;
-	// 	++it;
-	// }
-	// std::stack<int> s(mstack);
-	// return 0;
-
-
-
 	MutantStack<int>	mstack;
 
 	mstack.push(5);
@@ -53,7 +21,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
@@ -67,15 +34,17 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	
+
+
+	MutantStack<char>	ms;
+	
+	for(char i='a'; i<='z'; i++)
+		ms.push(i);
+	for(MutantStack<char>::iterator it=ms.begin(); it!=ms.end(); it++)
+		std::cout<<*it<<' ';
+	ms.push('A');
+	std::cout << *(--(ms.end())) << ' ';
 	return 0;
-
-	
-
-
-	// MutantStack<char>	ms;
-	
-	// for(char i='a'; i<='z'; i++)
-	// 	ms.push(i);
-	// for(MutantStack<char>::iterator it=ms.begin(); it!=ms.end(); it++)
-	// 	std::cout<<*it<<' ';
 }
